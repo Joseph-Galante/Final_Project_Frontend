2778e0fc82da0e50be20d764bc10480e86d28036
joseph-galante <jgalante00@gmail.com>
2021-05-26 17:40:21 -0400

    product details styling

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -35,6 +35,14 @@ const ProductDetails = (props) =>
     useEffect(clearMessage, [])
 
     // functions
+    const updateMenu = (tab) =>
+    {
+        setDisplay(tab)
+        document.querySelectorAll('.menu-item').forEach(item => {item.classList.remove('active')});
+        document.querySelector(`#${tab}`).classList.add('active')
+    }
+    useEffect(() => {updateMenu('description')}, [])
+
     const getProduct = () =>
     {
         axios.get(`${env.BACKEND_URL}/products/${props.productId}`).then((res) =>
@@ -79,83 +87,86 @@ const ProductDetails = (props) =>
         }, { headers: { Authorization: user.id}}).then((res) =>
         {
             getProduct()
-            setDisplay('reviews')
+            updateMenu('reviews')
             displayMessage(true, 'Review posted')
         }).catch(error => console.log(error.message))
     }
 
     return (
-        <div className="product-details">
+        <div className="product-details-page">
             {redirect !== '' ? <Redirect to={redirect} /> : null}
             {product ?
-                <div>
+                <div className="about-product">
                     {/* {console.log(product)} */}
-                    <h1>{product.name}</h1>
-                    <p>Price: ${product.price}</p>
-                    <p>Rating: {rating}</p>
-                    <p id="soldBy" onClick={() => {
-                        localStorage.setItem('revieweeId', product.seller.id);
-                        history.push(`/products/${product.id}`);
-                        setRedirect(`/reviews/users`);
-                    }}
-                    >Sold by: {product.seller ? product.seller.name : '(Unknown)'}</p>
+                    <div className="product-info">
+                        <div className="product-info-left">
+                            <img src={product.image} alt={product.name} width={350} height={350}/>
+                        </div>
+                        <div className="product-info-right">
+                            <h1>{product.name}</h1>
+                            <p>Price: ${product.price}</p>
+                            <p>Rating: {rating}</p>
+                            <p id="soldBy" onClick={() => {
+                                localStorage.setItem('revieweeId', product.seller.id);
+                                history.push(`/products/${product.id}`);
+                                setRedirect(`/reviews/users`);
+                            }}
+                            >Sold by: {product.seller ? product.seller.name : '(Unknown)'}</p>
 
-                    <span className="product-details-listing-add" onClick={() => {addToCart(product.id)}}>Add to Cart</span> 
+                            <span className="product-details-listing-add" onClick={() => {addToCart(product.id)}}>Add to Cart</span> 
+                        </div>
+                    </div>
 
-                    <div className="profileMenu">
-                        <div id="description" key="description" className="menuItem" onClick={() => {
+                    <div className="product-details-menu">
+                        <div id="description" key="description" className="menu-item" onClick={() => {
                             clearMessage();
-                            setDisplay('description')
-                            document.querySelectorAll('.menuItem').forEach(item => {item.classList.remove('active')});
-                            document.querySelector('#description').classList.add('active');
+                            updateMenu('description')
                         }}>
                             <h4>Description</h4>
                         </div>
-                        <div id="reviews" key="reviews" className="menuItem" onClick={() => {
+                        <div id="reviews" key="reviews" className="menu-item" onClick={() => {
                             
                             clearMessage();
-                            setDisplay('reviews')
-                            document.querySelectorAll('.menuItem').forEach(item => {item.classList.remove('active')});
-                            document.querySelector('#reviews').classList.add('active');
+                            updateMenu('reviews')
                         }}>
-                            <h4>Reviews</h4>
+                            <h4>Reviews ({product.reviews ? product.reviews.length : 0})</h4>
                         </div>
-                        <div id="leave-review" key="leave-review" className="menuItem" onClick={() => {
+                        <div id="leave-review" key="leave-review" className="menu-item" onClick={() => {
                             clearMessage();
-                            setDisplay('leave-review')
-                            document.querySelectorAll('.menuItem').forEach(item => {item.classList.remove('active')});
-                            document.querySelector('#leave-review').classList.add('active');
+                            updateMenu('leave-review')
                         }}>
                             <h4>Leave Review</h4>
                         </div>
                     </div>
-                    {display == 'description' ?
-                        <div>{product.description}</div>
-                        :
-                        display == 'reviews' ?
-                            product.reviews ? 
-                                product.reviews.length == 0 ?
-                                    'No reviews'
-                                    :
-                                    product.reviews.map(review =>
-                                    {
-                                        return (
-                                            <Review key={review.review.id} review={review.review}/>
-                                        )
-                                    })
-                                : 
-                                'Getting reviews...'
+                    <div className="product-details-display">
+                        {display == 'description' ?
+                            <div>{product.description}</div>
                             :
-                            display == 'leave-review' ?
-                                    <form className="review-form" onSubmit={(e) => {writeReview(e)}}>
-                                        <h1>Leave a Review</h1>
-                                        <input id="review-description-input" type="text" value={reviewDescription} placeholder="Write a review..." onChange={(e) => {setReviewDescription(e.target.value)}} />
-                                        <input id="review-rating-input" type="number" value={reviewRating} min={1} max={5} onChange={(e) => {setReviewRating(e.target.value)}} />
-                                        <input type="submit" value="Submit" />
-                                    </form>
-                                    :
-                                    null
-                    }
+                            display == 'reviews' ?
+                                product.reviews ? 
+                                    product.reviews.length == 0 ?
+                                        'No reviews'
+                                        :
+                                        product.reviews.map(review =>
+                                        {
+                                            return (
+                                                <Review key={review.review.id} review={review.review}/>
+                                            )
+                                        })
+                                    : 
+                                    'Getting reviews...'
+                                :
+                                display == 'leave-review' ?
+                                        <form className="review-form" onSubmit={(e) => {writeReview(e)}}>
+                                            <h1>Leave a Review</h1>
+                                            <input id="review-description-input" type="text" value={reviewDescription} placeholder="Write a review..." onChange={(e) => {setReviewDescription(e.target.value)}} />
+                                            <input id="review-rating-input" type="number" value={reviewRating} min={1} max={5} onChange={(e) => {setReviewRating(e.target.value)}} />
+                                            <input type="submit" value="Submit" />
+                                        </form>
+                                        :
+                                        null
+                        }
+                    </div>
                 </div>
                 :
                 'Getting product details...'
